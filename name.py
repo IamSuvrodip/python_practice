7a8fbf6d10f17c6ca5f0e9a2c1458ca271e1fed5
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-03-30 01:17:09 +0530

    Update name.py

--- name.py
+++ name.py
@@ -1,3 +1,29 @@
 def full_name(n):
     print(n)
 full_name("Suvrodip Chakroborty")
+
+name_one=("sumit")
+name_two=("anirban")
+name_three = ("dipendu")
+
+'''@IamSuvrodip ➜ /workspaces/codespaces-blank (main) $ python
+Python 3.10.13 (main, Mar 21 2024, 17:51:02) [GCC 9.4.0] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import name
+Suvrodip Chakroborty
+>>> name.name_one
+'sumit'
+>>> name.name_three
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: module 'name' has no attribute 'name_three'
+>>> from importlib import reload
+>>> name.name_three
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+AttributeError: module 'name' has no attribute 'name_three'
+>>> reload(name)
+Suvrodip Chakroborty
+<module 'name' from '/workspaces/codespaces-blank/name.py'>
+>>> name.name_three
+'dipendu'''
