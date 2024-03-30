0dedb1e875da38ad7d7daa10784b495f1951fdde
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-03-31 04:40:48 +0530

    Add files via upload

---
+++ 02_basics/datatypes.py
@@ -0,0 +1,51 @@
+# Object types / Data types
+'''
+list = []
+dictionary = {}
+tuple = ()
+file = open('test.txt')
+'''
+
+mylist = [123,"hello",3.1415]
+print('\n**********List***********\n')
+print(mylist)
+print(len(mylist))
+print(mylist[0])
+print(mylist[-1])
+
+mydict = {'one':'lemon','two':'sugar','three':'masala'} #one is key and lemon is value
+print('\n**********Dictionary***********\n')
+print(mydict)
+print(len(mydict))
+#print(mydict[0])-------X
+#print(mydict[-1])-------X
+print(mydict['two'])
+print(mydict['three'])
+
+mytuples = (1,2,3,4,5,6,7,8,9)
+print('\n**********Tuple***********\n')
+print(len(mytuples))
+print(mytuples[0])
+print(mytuples[-1])
+
+num=(0)
+print(type(num)) #<class 'int'>
+num=(0,)
+print(type(num)) #<class 'tuple'>
+
+print('\n**********Files***********\n')
+f= open("demo.txt")
+print(f)
+
+f= open("demo.txt",'w')
+data = f.write("Sumit kumar roy\n")
+
+f= open("demo.txt",'a')
+f.write("Suvrodip\n")
+f.write("Rudronil\n")
+
+
+f= open("demo.txt",'r')
+data = f.read()
+print(data)
+f.close()
---
+++ 02_basics/third.py
@@ -0,0 +1,22 @@
+'''Mutable data types (changes)
+    *list
+    *set
+    *dictionary
+    *bytearray
+    *array
+'''
+
+'''Immutable data types (does not changes)
+    *integers
+    *floating
+    *boolean
+    *strings
+    *tuples
+    *frozen set
+    *bytes
+'''
+
+username = "suvrodip01"
+
+
+
