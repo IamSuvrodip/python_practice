edc9bbb7776ecb98bf59f3102d692ec1419c8553
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-02 18:30:21 +0000

    first_problem

--- Conditional_problem/first_problem.py
+++ Conditional_problem/first_problem.py
@@ -1,5 +1,16 @@
 # Age Group Categorization
 
+age= int(input ("Enter Your Age= "))
+print(age)
+
+if age < 14:
+    print("You are Child..")
+elif age < 20:
+    print("You are Teenager..")
+elif age < 60:
+    print("You are Adult..")
+else:
+    print("You are Senior..")
 
 
 
