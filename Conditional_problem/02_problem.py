7a6f2e03e4d6e5514d34dec6a1addcf75bdb78fc
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-02 19:09:34 +0000

    02_problem

--- Conditional_problem/02_problem.py
+++ Conditional_problem/02_problem.py
@@ -1,5 +1,5 @@
 # Movie Ticket Pricing
-
+'''
 age = int(input("Enter Your age= "))
 day = (input("What the day today= ").lower())
 print("Your age is= ",age)
@@ -15,3 +15,16 @@ elif age>17 and day=='wednesday':
     print("You are Adult and today is wednesday So discount $2 So Your Movie ticket price is= $10")
 else:
     print("Something Wrong")
+'''
+
+age = int(input("Enter Your age= "))
+day = (input("What the day today= ").lower())
+print("Your age is= ",age)
+print("Today is= ",day)
+
+price = 12 if age>=18 else 8
+
+if day == "wednesday":
+    price = price-2
+
+print(f"Your movie ticket price is= {price}")
