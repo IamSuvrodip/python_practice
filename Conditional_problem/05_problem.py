63e1b96c3727d5c68e5e6709ddde5d538146c148
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-03 12:27:20 +0000

    05_problem

---
+++ Conditional_problem/05_problem.py
@@ -0,0 +1,13 @@
+# 5. Weather Activity Suggestion 
+
+weather = input("What the Weather Today Sunny/Rainy/Snowy: ").lower()
+
+if weather == 'sunny':
+    print("Go for a Walk")
+elif weather == 'rainy':
+    print("Read a Book")
+elif weather == 'snowy':
+    print("Build a Snowman")
+else:
+    print("You enter wrong Details")
+
