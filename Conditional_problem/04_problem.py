aa827355d2772511f3d0449e626beeac42fd7587
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-03 12:18:47 +0000

    04_problem

--- Conditional_problem/04_problem.py
+++ Conditional_problem/04_problem.py
@@ -1,13 +1,16 @@
 # Fruit Ripeness Checker
 
-print("The fruit is 'Banana'")
-fruit_condition = input("What is the Banana's condition (Green), (Yellow) or (Brown)= ").lower()
+fruit = input("What is your fruit's name= ").lower()
 
-if fruit_condition == 'green':
-    print("This Banana is Unripe")
-elif fruit_condition == 'yellow':
-     print("This Banana is Ripe")
-elif fruit_condition == 'brown':
-     print("This Banana is Overripe")
+if fruit == 'banana':
+    fruit_condition = input("What is the Banana's condition (Green), (Yellow) or (Brown)= ").lower()
+    if fruit_condition == 'green':
+        print("This Banana is Unripe")
+    elif fruit_condition == 'yellow':
+        print("This Banana is Ripe")
+    elif fruit_condition == 'brown':
+        print("This Banana is Overripe")
+    else:
+        print("You enterd wrong Details")
 else:
-    print("You enterd wrong Details")
+    print("You Choose Wrong Fruit")
