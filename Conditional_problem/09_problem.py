ab54ce592d3d3d767ff239aba18685499562ea46
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-04 18:34:31 +0000

    09_problem

--- Conditional_problem/09_problem.py
+++ Conditional_problem/09_problem.py
@@ -1,3 +1,8 @@
 # Leap Year Checker
 
 year = int(input("Enter the Year= "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("This year is leap year")
+else:
+    print("This year is not leap year")
