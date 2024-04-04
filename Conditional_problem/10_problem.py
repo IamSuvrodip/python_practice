055a36ac0c74dae1856dbc64866ea33d69a40c70
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-04 18:45:16 +0000

    10_problem

--- Conditional_problem/10_problem.py
+++ Conditional_problem/10_problem.py
@@ -1,1 +1,15 @@
-# 
+# Pet Food Recommendation
+
+pet =(input("Enter your pet's type dog/cat= ").lower())
+age = int(input("Enter your pet's age= "))
+
+if (pet == 'dog' and age<3):
+    print("Puppy food")
+elif (pet == 'dog' and age>2):
+    print("Adult dog food")
+elif (pet == 'cat' and age<6):
+    print("Junior cat food")
+elif (pet == 'cat' and age>5):
+    print("Senior cat food")
+else:
+    print("something wrong..")
