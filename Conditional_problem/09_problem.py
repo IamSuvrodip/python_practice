eb0f9706f8db4a2818139bd12b531cd2c57df331
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-04 18:36:52 +0000

    09_problem

--- Conditional_problem/09_problem.py
+++ Conditional_problem/09_problem.py
@@ -3,6 +3,6 @@
 year = int(input("Enter the Year= "))
 
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print("This year is leap year")
+    print(f"{year} This year is leap year")
 else:
-    print("This year is not leap year")
+    print(f"{year} This year is not leap year")
