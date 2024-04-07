e2d2805272093978d2c122c8e7f9c44fba628d91
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-07 17:04:51 +0000

    04_problem

--- Loops_problem/04_problem.py
+++ Loops_problem/04_problem.py
@@ -1,3 +1,11 @@
-#  Reverse a String
+#  Reverse a String using a loop
 
-  
+string = input("Enter your string: ")
+
+reversed_str = ""
+
+for char in string:
+    reversed_str = char +reversed_str 
+print(reversed_str)
+
+
