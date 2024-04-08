d1fbd86c8d4f81a3e228ec0ba9f172c74e6f1b01
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-08 19:00:30 +0000

    file_op

--- 08_basics iteration tools/file_operations.py
+++ 08_basics iteration tools/file_operations.py
@@ -1,5 +1,17 @@
 file1 = open("08_basics iteration tools/file.txt")
+'''
 print(file1.readline())
 print(file1.readline())
 print(file1.readline())
 print(file1.readline())
+'''
+'''print(file1.__next__())
+print(file1.__next__())
+print(file1.__next__())
+print(file1.__next__())'''
+
+for line in open('08_basics iteration tools/file.txt'):
+    print(line) # ---------> print next line gap
+    print(line, end='') # ------->print without next line gap
+
+
