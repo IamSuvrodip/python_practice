222a1ab66844dd13f4f40e8422ab3b029fac3908
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-08 05:52:31 +0000

    07_problem

---
+++ Loops_problem/07_problem.py
@@ -0,0 +1,10 @@
+# Validate Input
+
+while True:
+    number = int(input("Enter Your Number b/w 1 to 10: "))
+    if 1<= number <=10:
+        print(f"Your number is {number} Thanks")
+        break
+    else:
+        print("Please check your number")
+
