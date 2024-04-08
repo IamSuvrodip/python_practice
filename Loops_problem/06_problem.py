d76d6d0f28d9bc12d954e33f97d8185644473385
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-08 05:40:51 +0000

    06_problem

--- Loops_problem/06_problem.py
+++ Loops_problem/06_problem.py
@@ -1,6 +1,13 @@
 # Factorial Calculator using while loop
 
 number = int(input("Enter your number: "))
+factorial = int(input("Enter your factorial number: "))
+
+while number > 0:
+    factorial *= number
+    number = number - 1
+print(f"Factorial number of is= {factorial}")
 
 
+    
 
