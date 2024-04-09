05dfb516b1671b69ec33559804fa21205d135107
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-09 12:35:27 +0000

    file_op

--- 08_basics iteration tools/file_operations.py
+++ 08_basics iteration tools/file_operations.py
@@ -12,6 +12,30 @@ print(file1.__next__())'''
 
 for line in open('08_basics iteration tools/file.txt'):
     print(line) # ---------> print next line gap
+print('\n\n')
+
+for line in open('08_basics iteration tools/file.txt'):
     print(line, end='') # ------->print without next line gap
+print('\n\n')
+
+
+file1 = open("08_basics iteration tools/file.txt")
+while True:
+    line = file1.readline()
+    if not line:
+        break
+    print(line, end='')
+print('\n\n')
+
+name = "suvrodip"
+if not name:
+    print("rudronil")
+else:
+    print(name)
+print('\n\n')
+
+
+
+
 
 
