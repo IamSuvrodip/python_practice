2550c3a7d4a35c8d0159f63e94454fe5a22dd035
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-09 12:42:20 +0000

    basic_iter tools

--- 08_basics iteration tools/basics.py
+++ 08_basics iteration tools/basics.py
@@ -10,8 +10,26 @@
 # next()
 # next()
 
-import time
+'''import time
 
 print("suvrodip chakroborty")
 username = "Suvrodip"
-print(username)
+print(username)'''
+
+name = "suvrodip"
+if not name: #-------> Value not in the variable or list then this call true
+    print("rudronil")
+else:
+    print(name)
+print('\n\n')
+
+
+#iter tools
+my_list = [1, 2, 3, 4, 5]  #----->list
+I = iter(my_list)
+print(I) #-----------><list_iterator object at ---->0x7f33c6b5ff70<--- THIS IS MEMOEY LOCATION first memory referance
+print(I.__next__())
+print(I.__next__())
+print(I.__next__())
+print(I.__next__())
+print(I.__next__())
