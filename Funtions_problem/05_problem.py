8b6c6b7c82d8860de467c7656b3c7b82ddb6e4fe
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-11 05:30:57 +0000

    05_problem

--- Funtions_problem/05_problem.py
+++ Funtions_problem/05_problem.py
@@ -1,3 +1,6 @@
 # defult parameter value
 # write a function that greets a user. If no name is provided, it should greet with a default name
 
+def greet(name = "Rudronil"):
+    return "hello, " + name + " !"
+print(greet())
