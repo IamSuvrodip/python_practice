4af8a6e2f362c3b6baf7bed5b4f643f4450fc5bc
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-12 17:53:18 +0000

    10_problem

--- Funtions_problem/10_problem.py
+++ Funtions_problem/10_problem.py
@@ -1,6 +1,16 @@
 # Recursive function
 # calculate factorial number
 
+# speacial techniqe 
+# function ko call karteho bar bar functionke undersehi
 
+def add_num(a, b):
+    add_num(2,3) # ehape khudi khudiko call kar raha hein infite loop main fas geyi
 
-
+# how to exit to the recurtion this is main thing
+def factorial(n):
+    if n == 0: #--------->exit stratagy
+        return 1
+    else:
+        return n * factorial(n-1)
+print(factorial(5))
