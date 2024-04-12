7d1c7dc027064373213ca294884466e80fcf606d
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-12 16:41:31 +0000

    08_problem

--- Funtions_problem/08_problem.py
+++ Funtions_problem/08_problem.py
@@ -1,6 +1,11 @@
 # functions with **kwargs
+# beat for database
 
-def print_kwargs(name, address):
-    print("Name", name, "Address", address)
+def print_kwargs(**kwargs):
+    #print("Name", name, "Address", address)#------>X
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
 
 print_kwargs(name="Suvrodip", address="Basirhat")
+print_kwargs(name="Suvrodip")
+print_kwargs(address="Basirhat", phone=3543459)
