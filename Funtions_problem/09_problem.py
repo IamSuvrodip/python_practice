0e80ba89a1d12d7e41bc371824f46c275f7d22cf
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-12 17:41:21 +0000

    09_problem

--- Funtions_problem/09_problem.py
+++ Funtions_problem/09_problem.py
@@ -12,3 +12,16 @@ def even_generator(limit):
     for i in range(3, limit+1, 2):
        return(i)
 print(even_generator(10),'\n')
+
+def even_generator(limit):
+    li=[]
+    for i in range(0, limit+1, 2):
+       li.append(i)
+    return li
+print(even_generator(10),'\n\n')
+
+def even_generator(limit):
+    for i in range(0, limit+1, 2):
+       yield(i)
+for num in even_generator(10):
+    print(num)
