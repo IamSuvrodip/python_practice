c983396c4cd4400e5587d22cbe47db1cda643475
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-12 17:28:43 +0000

    09_problem

--- Funtions_problem/09_problem.py
+++ Funtions_problem/09_problem.py
@@ -2,3 +2,13 @@
 # yield concept is little bit dificult
 # but you think about this memory perspective then its easy for understand
 
+def even_generator(limit):
+    for i in range(0, limit+1, 2):
+        print(i)
+even_generator(10)
+print('\n')
+
+def even_generator(limit):
+    for i in range(3, limit+1, 2):
+       return(i)
+print(even_generator(10),'\n')
