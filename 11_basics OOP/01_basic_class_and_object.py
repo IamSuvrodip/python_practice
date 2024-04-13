d984364e9fb63386b158ebe4f73a249c2e12c389
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-13 12:12:03 +0000

    basic_class_and_object

--- 11_basics OOP/01_basic_class_and_object.py
+++ 11_basics OOP/01_basic_class_and_object.py
@@ -1,1 +1,11 @@
 # basic_class_and_object
+# create a Car class with attributes like brand and model. 
+# Then create an instance of this class.
+# there self and in java this keyword same
+
+class Car:
+    def __init__(self, brand, model): # ehape init likhunga to hum Car() k under parameter de sakte hain
+        self.brand = brand
+        self.model = model
+my_car = Car("Lmborgini", "New") # this Car and upar class bala Car connect karne keliye self keyword likhte hain
+print(my_car.brand,my_car.model)
