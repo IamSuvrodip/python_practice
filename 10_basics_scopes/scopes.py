ab9a7d1f9bea2e727652b179747248bf6cfae6b2
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-13 11:01:08 +0000

    scopes

--- 10_basics_scopes/scopes.py
+++ 10_basics_scopes/scopes.py
@@ -1,9 +1,20 @@
 # scopes
 
+# simple function
 name= "suvrodip"
-
 def username():
     name = "Dip"
     print(name)
 print(name)
 username()
+print('\n')
+
+name= "suvrodip"
+def username():
+    # name = "Dip"
+    print(name)
+print(name)
+username()
+print('\n')
+
+
