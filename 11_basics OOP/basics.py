5256203ab7e4841e1acea6007d2dbdad7e62a71a
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-13 12:21:50 +0000

    commit

--- 11_basics OOP/basics.py
+++
@@ -1,4 +0,0 @@
-from 01_basic_class_and_object import Car
-
-my_new_car = Car("Tata","Safari")
-print(my_new_car.brand,my_new_car.model)
---
+++ 11_basics OOP/import.py
@@ -0,0 +1,10 @@
+from class_object import Car
+
+my_new_car = Car("Tata","Safari")
+print(my_new_car.brand,my_new_car.model)
+
+my_new_car = Car("farari","Old")
+print(my_new_car.brand,my_new_car.model)
+
+my_new_car = Car("Thar","New")
+print(my_new_car.brand,my_new_car.model)
