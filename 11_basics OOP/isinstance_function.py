0baa18900f72912c23295e57b05667d48a6dcb1b
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-14 20:37:11 +0000

    project01

---
+++ 11_basics OOP/isinstance_function.py
@@ -0,0 +1,20 @@
+# class inheritance and isinstance() function
+# Demostrate the use of isinstance() to check if my_tesla is an instance
+# and Car and ElectricCar
+
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+    
+    def fullname(self):
+        return f"{self.brand} {self.model}"
+
+class ElectricCar(Car): #-----------> ElectricCar inherits all properties of Car
+    def __init__(self, brand, model, battery_size):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+my_car = ElectricCar("Tesla", " Model:-S", "85KWH")
+print(my_car.fullname())
+print(my_car.brand,my_car.model,my_car.battery_size)
