f27787b034765fc8ff4fa95d72c61740bd99239a
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-15 11:08:08 +0000

    files operations

--- basics+++/files.py
+++ basics+++/files.py
@@ -1,3 +1,13 @@
 # files operations
 
 file = open('test.txt','w')
+
+# this one tupe
+try:
+    file.write("Suvrodip")
+finally:
+    file.close()
+
+# this is another type
+with open('./project/youtube.txt', 'w') as file:
+    file.write("rudronil")
