d240d46d40eff17971b0bcc575b35ceef96af8d8
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-15 05:22:15 +0000

    debugging_function_call

--- Decorators_basics/debugging_function_call.py
+++ Decorators_basics/debugging_function_call.py
@@ -4,21 +4,22 @@
 
 def debug(func):
     def wrapper(*args, **kwargs):
-        a
-        return func(*args, **kwargs)
-    
-        
+        args_value = ', '.join(str(arg) for arg in args)#-------give iterable list
+        kwargs_value = ', '.join(f"{k} = {v}"for k, v in kwargs.items())
+        print(f"calling:function name= ({func.__name__}) with args= ({args_value}) and kwargs= ({kwargs_value})")
+
+        return func(*args, **kwargs)  
     return wrapper
 
 
+@debug
+def hello():
+    print("hello\n")
 
 
-
-
-
-
-
+@debug
 def greet(name, greeting = "Hello"):
     print(f"{greeting} {name}")
 
+hello()
 greet("Suvrodip", greeting= "Hi...")#------>replace hello to Hi....
